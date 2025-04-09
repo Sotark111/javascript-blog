c7a1fca4676ec0d09a07d238c7abc1912f2680a4
Adam Wardak <adamwa12@interia.pl>
2025-04-09 20:49:14 +0200

    blog

--- js/script.js
+++ js/script.js
@@ -29,14 +29,19 @@ document.getElementById('test-button').addEventListener('click', function(){
     for(let activeArticle of activeArticles){
         activeArticle.classList.remove('active');
     }
-    /* get 'href' attribute from the clicked link */
+    /*[IN PROGRESS] get 'href' attribute from the clicked link */
 
     const articleSelector = clickedElement.getAttribute('href');
     console.log(articleSelector);
   
-    /* find the correct article using the selector (value of 'href' attribute) */
-  
-    /* add class 'active' to the correct article */
+    /* [IN PROGRESS]find the correct article using the selector (value of 'href' attribute) */
+
+    const targetArticle = document.querySelector(articleSelector);
+    console.log('targetArticle:', targetArticle);
+
+
+    /*[IN PROGRESS] add class 'active' to the correct article */
+    targetArticle.classList.add('active');
   }
   
   const links = document.querySelectorAll('.titles a');
